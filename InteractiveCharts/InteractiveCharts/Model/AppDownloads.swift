64e45ecfb103a76843902eefe5dbf1441cb490cf
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-29 06:20:55 +0200

    add date extension for creating date

--- InteractiveCharts/InteractiveCharts/Model/AppDownloads.swift
+++ InteractiveCharts/InteractiveCharts/Model/AppDownloads.swift
@@ -34,3 +34,17 @@ extension [AppDownloads] {
         return 0
     }
 }
+
+extension Date {
+    static func createDate(_ day: Int, _ month: Int, _ year: Int) -> Date {
+        var components = DateComponents()
+        components.day = day
+        components.month = month
+        components.year = year
+        
+        let calendar = Calendar.current
+        let date = calendar.date(from: components) ?? .init()
+        
+        return date
+    }
+}
