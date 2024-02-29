eba725e5e76ca27e415401442aed36864c913923
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-02-29 06:08:42 +0200

    init project

---
+++ InteractiveCharts/InteractiveCharts/InteractiveChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveChartsApp.swift
+//  InteractiveCharts
+//
+//  Created by OLEKSANDR ISAIEV on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
