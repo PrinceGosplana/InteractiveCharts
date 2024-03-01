c2317f28a08651e32bde410774b7ae4905f19b4d
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-01 06:30:48 +0200

    show home view on content view

--- InteractiveCharts/InteractiveCharts/View/HomeView.swift
+++ InteractiveCharts/InteractiveCharts/View/HomeView.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+        }
     }
 }
 
 #Preview {
-    HomeView()
+    ContentView()
 }
