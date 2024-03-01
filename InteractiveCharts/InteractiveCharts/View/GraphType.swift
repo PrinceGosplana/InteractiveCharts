f344bb0c25a108f19f5021d8583fbffb59f6b12a
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-01 06:31:40 +0200

    add graph view

---
+++ InteractiveCharts/InteractiveCharts/View/GraphType.swift
@@ -0,0 +1,18 @@
+//
+//  GraphType.swift
+//  InteractiveCharts
+//
+//  Created by OLEKSANDR ISAIEV on 01.03.2024.
+//
+
+import SwiftUI
+
+struct GraphType: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GraphType()
+}
